f94ada3cfa14d0cc939fd2e84b90d3212bf72231
Mukul <mukulsmart26@gmail.com>
2024-01-19 23:01:57 +0530

    Quiz Question layout setup done

--- client/src/Components/QuizQuestion/QuizQuestion.js
+++ client/src/Components/QuizQuestion/QuizQuestion.js
@@ -4,7 +4,26 @@ import styles from './QuizQuestion.module.css'
 function QuizQuestion({quiz,setQuiz}) {
   return (
     <div className={styles.quiz_form}>
-      
+      <div className={styles.row_1}>
+
+      </div>
+      <div className={styles.row_2}>
+
+      </div>
+      <div className={styles.row_3}>
+
+      </div>
+      <div className={styles.options_container}>
+        <div className={styles.col_1}>
+
+        </div>
+        <div className={styles.col_2}>
+
+        </div>
+      </div>
+      <div className={styles.button_box}>
+        
+      </div>
     </div>
   )
 }
